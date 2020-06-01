69d598eb6dc722a206f8f59b961ad16e00532349
Ben Chen <g4ru04@gmail.com>
2020-06-01 23:16:04 +0800

    feat: add 404 handling

--- routes/users.js
+++ routes/users.js
@@ -2,24 +2,32 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 /* GET users listing. */
+axios.defaults.validateStatus = function validateStatus(){
+  return true;
+}
 router.get('/', async function(req, res, next) {
   // console.log(req.query);
   try{
-    const urlResult = await axios.get(Buffer.from(req.query.url, 'base64').toString('ascii'),{
+    const transUrl = Buffer.from(req.query.url, 'base64').toString('ascii');
+    console.log(transUrl);
+    if (transUrl.includes('whatismyip')) {
+      let ipRes = await axios.get(transUrl);
+      res.send(ipRes.data);
+      return ;
+    }
+
+    const urlResult = await axios.get(transUrl,{
       headers: {
         'User-Agent': req.headers['user-agent'],
         'Content-Language': 'en-US',
         'host': 'www.instagram.com'
       }
-    })
-    // console.log(urlResult);
-    res.send(urlResult.data);
+    });
+    // console.log(urlResult.status);
+    res.status(urlResult.status).send(urlResult.data);
   }catch(err){
     res.send(err.message);
   }
 });
 
 module.exports = router;
-
-
-
